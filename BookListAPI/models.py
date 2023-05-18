869a49c48020e8c55cbf49a7fe10216ce6717a5d
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-05-18 11:31:21 +0100

    update on views

--- BookListAPI/models.py
+++ BookListAPI/models.py
@@ -8,10 +8,10 @@ class Book (models.Model):
     author = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=5, decimal_places=2)
 
-    class Meta:
-        indexes = [
-            models.Index(fields=['price']),
-        ]
+    # class Meta:
+    #     indexes = [
+    #         models.Index(fields=['price']),
+    #     ]
 
     def __str__(self):
         return self.title
--- BookListAPI/urls.py
+++ BookListAPI/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 # Add URL configuration for the path() function here
-    path('books', views.books),
-    # path('books/<int:pk>',views.book),
+    path('books', views.BookView.as_view()),
+    path('books/<int:pk>', views.SingleBookView.as_view()),
 ]
